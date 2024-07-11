12c776b120664241377a74578684af19f6ec6ef0
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-07-11 21:07:19 +0600

    check id and title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,8 +31,8 @@ export const Home = () => {
         <Grid xs={8} item>
           {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
+              id={obj._id}
+              title={obj.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
               user={{
                 avatarUrl:

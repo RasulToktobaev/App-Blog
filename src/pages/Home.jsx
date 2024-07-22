58f06e59735cfd6c7e73bfbb8b27c50751e8cea7
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-07-22 19:41:02 +0600

    add obj user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,23 +29,19 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? (
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => 
+          isPostsLoading ? (
             <Post key={index}  isLoading={true} />
           ) : (
             <Post
               id={obj._id}
               title={obj.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
+              user={obj.user}
               createdAt={'12 июня 2022 г.'}
               viewsCount={150}
               commentsCount={3}
               tags={['javascript', 'fun', 'typescript']}
-            
               isEditable
             />
             ),

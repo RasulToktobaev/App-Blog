d20639c33c873df0b5ba268d5a9a3cc05724df95
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-07-31 22:39:23 +0600

    create Slice

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,8 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchUserData = createAsyncThunk('auth/fetchUserData', async () => {
-    const { data } = await axios.post('/auth/login')
+export const fetchUserData = createAsyncThunk('auth/fetchUserData', async (params) => {
+    const { data } = await axios.post('/auth/login', params)
     return data;
 });
 
@@ -14,5 +14,20 @@ const initialState = {
 };
 
 const authSlice = createSlice({
-
+    name:'auth',
+    initialState,
+    extraReducers: {
+        [fetchUserData.pending]: (state) => {
+           state.status = 'loading';
+            state.data = null;
+        },
+        [fetchUserData.fulfilled]: (state, action) => {
+            state.status ='loaded';
+            state.data = action.payload;
+        },
+        [fetchUserData.rejected]: (state) => {
+            state.status = 'error';
+            state.data = null;
+        }
+    }
 })

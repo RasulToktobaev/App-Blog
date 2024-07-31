a3d6dee915a29e982c91d95ae66aec30bf859b97
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-07-31 22:33:37 +0600

    create functionality for authorizations

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,1 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUserData = createAsyncThunk('auth/fetchUserData', async () => {
+    const { data } = await axios.post('/auth/login')
+    return data;
+});
+
+
+
+const initialState = {
+    data: null,
+    status: 'loading',
+};
+
+const authSlice = createSlice({
+
+})

31221b1ae04b4862201027b978fc44064da8e590
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-02 22:36:46 +0600

    delete Route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Container maxWidth="lg">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
           <Route path="/posts/:id" element={<FullPost />} />
           <Route path="/add-post" element={<AddPost />} />
           <Route path="/login" element={<Login />} />

662d9dfab8a16c2018f40556e3b4c24b46096788
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-15 00:00:36 +0600

    added one more check

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -23,10 +23,13 @@ export const Login = () => {
 
   const onSubmit = async (values) => {
     const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert('Не удалось авторизоваться !')
+    }
+
     if ('token' in data.payload) {
       window.localStorage.setItem('token', data.payload.token);
-    } else {
-        alert('Не удалось авторизоваться !')
     }
 
 

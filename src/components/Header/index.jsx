01cd0731a39619df86aec23029183ac8199f0ea9
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-14 23:46:36 +0600

    we do the action of logging out of the account

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,18 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
-import { selectIsAuth } from "../../redux/slices/auth";
-import { useSelector } from 'react-redux';
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth)
 
-  const onClickLogout = () => { };
+  const onClickLogout = () => {
+   if(window.confirm('Вы действительно хотите выйти ? ')) {
+     dispatch(logout());
+    }
+   };
 
   return (
     <div className={styles.root}>

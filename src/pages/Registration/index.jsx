b990f07dcfdd9f1dffa1405ae2111b30df2b02a0
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-19 23:12:59 +0600

    add fullName in user

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -20,6 +20,8 @@ export const Registration = () => {
     mode: "onChange"
   });
 
+  
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">

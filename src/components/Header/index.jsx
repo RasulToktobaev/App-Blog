a4b4bcc12ce6571f3fbe99424a22a7a4871f1cf3
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-23 23:27:40 +0600

    change the path in the header to the post

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import styles from './Header.module.scss';
-import Container from '@mui/material/Container';
-import { logout, selectIsAuth } from "../../redux/slices/auth";
-import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout, selectIsAuth } from '../../redux/slices/auth'
+import styles from './Header.module.scss'
 
 export const Header = () => {
-  const dispatch = useDispatch();
-  const isAuth = useSelector(selectIsAuth)
+	const dispatch = useDispatch()
+	const isAuth = useSelector(selectIsAuth)
 
-  const onClickLogout = () => {
-   if(window.confirm('Вы действительно хотите выйти ? ')) {
-     dispatch(logout());
-     window.localStorage.removeItem('token')
-    }
-   };
+	const onClickLogout = () => {
+		if (window.confirm('Вы действительно хотите выйти ? ')) {
+			dispatch(logout())
+			window.localStorage.removeItem('token')
+		}
+	}
 
-  return (
-    <div className={styles.root}>
-      <Container maxWidth="lg">
-        <div className={styles.inner}>
-          <Link className={styles.logo} to="/">
-            <div>TOKTOBAEV BLOG</div>
-          </Link>
-          <div className={styles.buttons}>
-            {isAuth ? (
-              <>
-                <Link to="/posts/create">
-                  <Button variant="contained">Написать статью</Button>
-                </Link>
-                <Button onClick={onClickLogout} variant="contained" color="error">
-                  Выйти
-                </Button>
-              </>
-            ) : (
-              <>
-                <Link to="/login">
-                  <Button variant="outlined">Войти</Button>
-                </Link>
-                <Link to="/register">
-                  <Button variant="contained">Создать аккаунт</Button>
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
-      </Container>
-    </div>
-  );
-};
+	return (
+		<div className={styles.root}>
+			<Container maxWidth="lg">
+				<div className={styles.inner}>
+					<Link className={styles.logo} to="/">
+						<div>TOKTOBAEV BLOG</div>
+					</Link>
+					<div className={styles.buttons}>
+						{isAuth ? (
+							<>
+								<Link to="/add-post">
+									<Button variant="contained">Написать статью</Button>
+								</Link>
+								<Button
+									onClick={onClickLogout}
+									variant="contained"
+									color="error"
+								>
+									Выйти
+								</Button>
+							</>
+						) : (
+							<>
+								<Link to="/login">
+									<Button variant="outlined">Войти</Button>
+								</Link>
+								<Link to="/register">
+									<Button variant="contained">Создать аккаунт</Button>
+								</Link>
+							</>
+						)}
+					</div>
+				</div>
+			</Container>
+		</div>
+	)
+}

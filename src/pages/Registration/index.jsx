53a92505b9437b7acb7e6458c3e1ab47c30d4686
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-23 23:15:12 +0600

    change alert message in registration

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,72 +1,93 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import React from 'react';
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { fetchRegister, selectIsAuth } from "../../redux/slices/auth";
-import styles from './Login.module.scss';
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import { fetchRegister, selectIsAuth } from '../../redux/slices/auth'
+import styles from './Login.module.scss'
 
 export const Registration = () => {
-  const isAuth = useSelector(selectIsAuth)
-  const dispatch = useDispatch();
-  const { register, handleSubmit, formState: { errors, isValid },
-  } = useForm({
-    defaultValues: {
-      fullName: "Вася Пупкин",
-      email: "vasya@test.ru",
-      password: "1234",
-    },
-    mode: "onChange"
-  });
+	const isAuth = useSelector(selectIsAuth)
+	const dispatch = useDispatch()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			fullName: 'Вася Пупкин',
+			email: 'vasya@test.ru',
+			password: '1234',
+		},
+		mode: 'onChange',
+	})
 
+	const onSubmit = async (values) => {
+		const data = await dispatch(fetchRegister(values))
 
+		if (!data.payload) {
+			return alert('Не удалось зарегистрироваться  !')
+		}
 
-  const onSubmit = async (values) => {
-    const data = await dispatch(fetchRegister(values));
+		if ('token' in data.payload) {
+			window.localStorage.setItem('token', data.payload.token)
+		}
 
-    if (!data.payload) {
-      return alert('Не удалось авторизоваться !')
-    }
+		if (isAuth) {
+			return <Navigate to="/" />
+		}
 
-    if ('token' in data.payload) {
-      window.localStorage.setItem('token', data.payload.token);
-    }
-
-    if (isAuth) {
-      return <Navigate to="/" />;
-    }
-
-
-    return (
-      <Paper classes={{ root: styles.root }}>
-        <Typography classes={{ root: styles.title }} variant="h5">
-          Создание аккаунта
-        </Typography>
-        <div className={styles.avatar}>
-          <Avatar sx={{ width: 100, height: 100 }} />
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField error={Boolean(errors.fullName?.message)}
-            helperText={errors.fullName?.message}
-            type="email"
-            {...register("fullName", { required: "Укажите полное имя" })} className={styles.field} label="Полное имя" fullWidth />
-          <TextField error={Boolean(errors.email?.message)}
-            helperText={errors.email?.message}
-            type="email"
-            {...register("email", { required: "Укажите почту" })} className={styles.field} label="E-Mail" fullWidth />
-          <TextField error={Boolean(errors.password?.message)}
-            helperText={errors.password?.message}
-            type="password"
-            {...register("password", { required: "Укажите пароль" })} className={styles.field} label="Пароль" fullWidth />
-          <Button disabled={isValid} type='submit' size="large" variant="contained" fullWidth>
-            Зарегистрироваться
-          </Button>
-        </form>
-      </Paper>
-    );
-  };
+		return (
+			<Paper classes={{ root: styles.root }}>
+				<Typography classes={{ root: styles.title }} variant="h5">
+					Создание аккаунта
+				</Typography>
+				<div className={styles.avatar}>
+					<Avatar sx={{ width: 100, height: 100 }} />
+				</div>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<TextField
+						error={Boolean(errors.fullName?.message)}
+						helperText={errors.fullName?.message}
+						type="email"
+						{...register('fullName', { required: 'Укажите полное имя' })}
+						className={styles.field}
+						label="Полное имя"
+						fullWidth
+					/>
+					<TextField
+						error={Boolean(errors.email?.message)}
+						helperText={errors.email?.message}
+						type="email"
+						{...register('email', { required: 'Укажите почту' })}
+						className={styles.field}
+						label="E-Mail"
+						fullWidth
+					/>
+					<TextField
+						error={Boolean(errors.password?.message)}
+						helperText={errors.password?.message}
+						type="password"
+						{...register('password', { required: 'Укажите пароль' })}
+						className={styles.field}
+						label="Пароль"
+						fullWidth
+					/>
+					<Button
+						disabled={isValid}
+						type="submit"
+						size="large"
+						variant="contained"
+						fullWidth
+					>
+						Зарегистрироваться
+					</Button>
+				</form>
+			</Paper>
+		)
+	}
 }

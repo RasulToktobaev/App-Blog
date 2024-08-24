02baa1e04df9c066ec5cd77ebaf839329b661e7f
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-08-24 21:59:59 +0600

    change password in login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,68 +1,78 @@
-import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
-import styles from "./Login.module.scss";
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import { fetchAuth, selectIsAuth } from '../../redux/slices/auth'
+import styles from './Login.module.scss'
 
 export const Login = () => {
-  const isAuth = useSelector(selectIsAuth)
-  const dispatch = useDispatch();
-  const { register, handleSubmit,  formState: { errors, isValid },
-  } = useForm({
-    defaultValues: {
-      email: "test@test.ru",
-      password: "123",
-    },
-    mode: "onChange"
-  });
-    
-  const onSubmit = async (values) => {
-    const data = await dispatch(fetchAuth(values));
+	const isAuth = useSelector(selectIsAuth)
+	const dispatch = useDispatch()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			email: 'test@test.ru',
+			password: '12345',
+		},
+		mode: 'onChange',
+	})
 
-    if (!data.payload) {
-      return alert('Не удалось авторизоваться !')
-    }
+	const onSubmit = async (values) => {
+		const data = await dispatch(fetchAuth(values))
 
-    if ('token' in data.payload) {
-      window.localStorage.setItem('token', data.payload.token);
-    }
+		if (!data.payload) {
+			return alert('Не удалось авторизоваться !')
+		}
 
-    if (isAuth) {
-      return <Navigate to="/" />;
-    }
+		if ('token' in data.payload) {
+			window.localStorage.setItem('token', data.payload.token)
+		}
 
-    return (
-      <Paper classes={{ root: styles.root }}>
-        <Typography classes={{ root: styles.title }} variant="h5">
-          Вход в аккаунт
-        </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            className={styles.field}
-            label="E-Mail"
-            error={Boolean(errors.email?.message)}
-            helperText={errors.email?.message}
-            type="email"
-            {...register("email", { required: "Укажите почту" })}
-            fullWidth
-          />
-          <TextField className={styles.field}
-            label="Пароль"
-            error={Boolean(errors.password?.message)}
-            helperText={errors.password?.message}
-            {...register("password", { required: "Укажите пароль" })}
-            fullWidth
-          />
-          <Button disabled={isValid}  type="submit" size="large" variant="contained" fullWidth>
-            Войти
-          </Button>
-        </form>
-      </Paper>
-    );
-  };
+		if (isAuth) {
+			return <Navigate to="/" />
+		}
+
+		return (
+			<Paper classes={{ root: styles.root }}>
+				<Typography classes={{ root: styles.title }} variant="h5">
+					Вход в аккаунт
+				</Typography>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<TextField
+						className={styles.field}
+						label="E-Mail"
+						error={Boolean(errors.email?.message)}
+						helperText={errors.email?.message}
+						type="email"
+						{...register('email', { required: 'Укажите почту' })}
+						fullWidth
+					/>
+					<TextField
+						className={styles.field}
+						label="Пароль"
+						error={Boolean(errors.password?.message)}
+						helperText={errors.password?.message}
+						{...register('password', { required: 'Укажите пароль' })}
+						fullWidth
+					/>
+					<Button
+						disabled={isValid}
+						type="submit"
+						size="large"
+						variant="contained"
+						fullWidth
+					>
+						Войти
+					</Button>
+				</form>
+			</Paper>
+		)
+	}
 }

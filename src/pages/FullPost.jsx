3fbf7ace6a33fee53487de65ea1b060543aa28e1
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-09-10 23:40:01 +0600

    replacing the paragraph tag with a react component

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,69 +1,71 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { Post } from "../components/Post";
-import { Index } from "../components/AddComment";
-import { CommentsBlock } from "../components/CommentsBlock";
-import axios from "axios";
+import axios from 'axios'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import { useParams } from 'react-router-dom'
+import { Index } from '../components/AddComment'
+import { CommentsBlock } from '../components/CommentsBlock'
+import { Post } from '../components/Post'
 
 export const FullPost = () => {
+	const [data, setData] = React.useState()
+	const [isLoading, setIsLoading] = React.useState(true)
 
-  const [data, setData] = React.useState();
-  const [isLoading, setIsLoading] = React.useState(true);
+	const { id } = useParams()
 
-  const { id } = useParams()
+	React.useEffect(() => {
+		axios
+			.get(`/posts/${id}`)
+			.then((res) => {
+				setData(res.data)
+				setIsLoading(false)
+			})
+			.catch((err) => {
+				console.warn(err)
+				alert('Ошибка при получений статьи')
+			})
+	}, [])
 
-  React.useEffect(() => {
-    axios.get(`/posts/${id}`).then(res => {
-      setData(res.data)
-      setIsLoading(false)
-    }).catch(err => {
-      console.warn(err);
-      alert('Ошибка при получений статьи')
-    });
-  }, [])
+	if (isLoading) {
+		return <Post isLoading={isLoading} isFullPost />
+	}
 
-  if (isLoading) {
-    return <Post isLoading={isLoading} isFullPost />
-  }
-
-
-  return (
-    <>
-      <Post
-        id={data._id}
-        title={data.title}
-        imageUrl={data.imageUrl}
-        //imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={data.user}
-        createdAt={data.createdAt}
-        viewsCount={data.viewsCount}
-        commentsCount={3}
-        tags={data.tags}
-        isFullPost
-      >
-        <p> {data.text}</p>
-      </Post>
-      <CommentsBlock
-        items={[
-          {
-            user: {
-              fullName: "Вася Пупкин",
-              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
-            },
-            text: "Это тестовый комментарий 555555",
-          },
-          {
-            user: {
-              fullName: "Иван Иванов",
-              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
-            },
-            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
-          },
-        ]}
-        isLoading={false}
-      >
-        <Index />
-      </CommentsBlock>
-    </>
-  );
-};
+	return (
+		<>
+			<Post
+				id={data._id}
+				title={data.title}
+				imageUrl={data.imageUrl}
+				//imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+				user={data.user}
+				createdAt={data.createdAt}
+				viewsCount={data.viewsCount}
+				commentsCount={3}
+				tags={data.tags}
+				isFullPost
+			>
+				<ReactMarkdown children={data} />
+			</Post>
+			<CommentsBlock
+				items={[
+					{
+						user: {
+							fullName: 'Вася Пупкин',
+							avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+						},
+						text: 'Это тестовый комментарий 555555',
+					},
+					{
+						user: {
+							fullName: 'Иван Иванов',
+							avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+						},
+						text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+					},
+				]}
+				isLoading={false}
+			>
+				<Index />
+			</CommentsBlock>
+		</>
+	)
+}

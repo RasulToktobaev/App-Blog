49efd92fba15ebf88dc0da4b814d4777057cd373
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-09-11 23:47:44 +0600

    delete from redux article

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -30,6 +30,8 @@ const postSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: {
+
+        // Получение статей
         [fetchPosts.pending]: (state) => {
             state.posts.items = [];
             state.posts.status = 'loading';
@@ -42,6 +44,8 @@ const postSlice = createSlice({
             state.posts.items = [];
             state.posts.status = 'error';
         },
+
+        // Получение тегов
         [fetchTags.pending]: (state) => {
             state.tags.items = [];
             state.tags.status = 'loading';
@@ -51,6 +55,20 @@ const postSlice = createSlice({
             state.tags.status = 'loaded';
         },
         [fetchTags.rejected]: (state) => {
+            state.tags.items = [];
+            state.tags.status = 'error';
+        },
+
+        // Удаление статьи
+        [fetchRemovePost.pending]: (state, action) => {
+            state.posts.items = state.posts.items.filter(obj => obj._id === action.payload)
+
+        },
+        [fetchRemovePost.fulfilled]: (state, action) => {
+            state.tags.items = action.payload;
+            state.tags.status = 'loaded';
+        },
+        [fetchRemovePost.rejected]: (state) => {
             state.tags.items = [];
             state.tags.status = 'error';
         },
